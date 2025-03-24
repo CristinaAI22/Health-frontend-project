4cdb4c6d27bf346fe1e19c740a92775ff6a374ff
CristinaAI22 <cristina.ionica22@gmail.com>
2025-03-24 14:57:12 +0200

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind + Vite (noua metodă) 🎯
+      </h1>
+    </div>
+  );
+}
